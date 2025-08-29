57cdee0ebbd56ba5363e57ff906ae547997e81c9
raj <rajtech645@gmail.com>
2025-08-29 09:22:28 +0530

    admin orders

--- client/src/admin/Orders.tsx
+++ client/src/admin/Orders.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 type Props = {}
 
 const Orders = (props: Props) => {
-  return (
-    <div>Orders</div>
-  )
+    return (
+        <div className='max-w-6xl mx-auto my-10'>
+            iohjg
+        </div>
+    )
 }
 
 export default Orders
