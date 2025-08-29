9aff97b8a39e9ff3a294d1a3cf23474ad1dd5cb1
raj <rajtech645@gmail.com>
2025-08-29 09:19:21 +0530

    admin orders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,6 +18,7 @@ import RestaurantDetail from './components/RestaurantDetail'
 import Cart from './components/Cart'
 import Restaurant from './admin/Restaurant'
 import AddMenu from './admin/AddMenu'
+import Orders from './admin/Orders'
 
 const appRouter = createBrowserRouter([
   {
@@ -48,9 +49,13 @@ const appRouter = createBrowserRouter([
         path: "/admin/restaurant",
         element: <Restaurant />
       },
-          {
+      {
         path: "/admin/menu",
         element: <AddMenu />
+      },
+      {
+        path: "/admin/orders",
+        element: <Orders />
       }
     ]
 
